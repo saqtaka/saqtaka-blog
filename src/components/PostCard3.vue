d05d682be821a10e710bb4a287aa3b12786dc96d
saqtaka <50040766+saqtaka@users.noreply.github.com>
2021-04-11 01:09:00 +0900

    Update src/components/PostCard3.vue

--- src/components/PostCard3.vue
+++ src/components/PostCard3.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="post-card pa-2" height="330" rounded="xl" :to="post.path">
-    <v-img v-if="post.cover_image" class="ma-2 rounded" :src="post.cover_image" />
+    <!-- <v-img v-if="post.cover_image" class="ma-2 rounded" :src="post.cover_image" /> -->
     <v-sheet class="ma-3" :height="70">
       <div class="text-body-2">{{ post.title }}</div>
     </v-sheet>
