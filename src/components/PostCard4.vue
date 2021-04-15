974ba45dc8254cedb7135f7eb6e5d47a1f733e52
saqtaka <50040766+saqtaka@users.noreply.github.com>
2021-04-15 11:25:53 +0900

    new

---
+++ src/components/PostCard4.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <!-- <v-card class="post-card" height="350" rounded="xl" :to="post.path"> -->
+      <div>
+        <v-img v-if="post.cover_image" class="rounded" :src="post.cover_image" />
+        <!-- <v-sheet class="ma-3" :height="100"> -->
+          <div class="text-body-2">{{ post.title }}</div>
+        <!-- </v-sheet> -->
+        <div class="ma-3" >
+          <!-- <PostMeta :post="post" /> -->
+          <PostTags :post="post" />
+        </div>
+      </div>
+    <!-- </v-card> -->
+  </div>
+</template>
+
+<script>
+// import PostMeta from '~/components/PostMeta'
+import PostTags from '~/components/PostTags'
+
+export default {
+  components: {
+    // PostMeta,
+    PostTags
+  },
+  props: ['post']
+}
+</script>
+
+<style lang="scss">
+.post-card {
+  margin-bottom: var(--space);
+  position: relative;
+
+  &__header {
+    margin-left: calc(var(--space) * -1);
+    margin-right: calc(var(--space) * -1);
+    margin-bottom: calc(var(--space) / 2);
+    margin-top: calc(var(--space) * -1);
+    overflow: hidden;
+    border-radius: var(--radius) var(--radius) 0 0;
+
+    &:empty {
+      display: none;
+    }
+  }
+
+  &__image {
+    min-width: 100%;
+  }
+
+  &__title {
+    margin-top: 0;
+  }
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 1px 10px 30px 0 rgba(0,0,0,.1);
+  }
+
+  &__tags {
+    z-index: 1;
+    position: relative;
+  }
+
+  &__link {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.0;
+    overflow: hidden;
+    text-indent: -9999px;
+    z-index: 0;
+  }
+}
+</style>
---
+++ src/layouts/Default2.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="white"
+      elevate-on-scroll
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+      <v-main><!-- アプリケーションに適切なgutterを提供します -->
+    <!-- <v-sheet id="scrolling-techniques-7" class="overflow-y-auto" max-height="600"> -->
+        <v-container fluid><!-- vue-routerを使用している場合 -->
+          <!-- <router-view></router-view> -->
+          <slot />
+        </v-container>
+    <!-- </v-sheet> -->
+      </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
