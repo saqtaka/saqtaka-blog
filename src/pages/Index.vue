b9c73e64f93f30839d78dce50d66d4efd959cc22
make-ship <50040766+make-ship@users.noreply.github.com>
2021-04-16 10:12:31 +0900

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -22,7 +22,7 @@ query {
       node {
         id
         title
-        date (format: "D. MMMM YYYY")
+        date (format: "YYYY/MM/DD")
         timeToRead
         description
         cover_image (width: 770, height: 380, blur: 10)
